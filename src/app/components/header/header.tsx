8f9fdccec4231953a1e20c5960a06d663b6f8098
Wenderson Junior <wenderson.junior@onovomercado.com.br>
2025-06-05 15:06:41 -0300

    feat: add language switcher component and update header for i18n support

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,21 +1,46 @@
-import { Container } from "@/app/components/ui/Container";
+"use client";
+
+import { Container } from "../ui/Container";
+import { Logo } from "./Logo";
+import { NavItems } from "./NavItems";
+import { MobileMenu } from "./MobileMenu";
+import { LanguageSwitcher } from "./LanguageSwitcher";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [scrolled, setScrolled] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const offset = window.scrollY;
+    setScrolled(offset > 0);
+    
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      setScrolled(offset > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="w-full h-17">
+    <header
+      className={`w-full h-17 fixed top-0 z-50 ${
+        scrolled === true ? "border-b border-transparent dark:border-gray-900 dark:bg-gray/50 backdrop-blur-xl shadow-sm" : ""
+      }`}
+    >
       <Container>
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0">
-            <h3 className="text-2xl font-bold dark:text-white">
-              {"<WJR />"}
-            </h3>
+            <Logo />
           </div>
-          <nav className="hidden sm:flex items-center space-x-8">
-            <a href="#" className="dark:text-gray-300 dark:hover:text-white">Home</a>
-            <a href="#" className="dark:text-gray-300 dark:hover:text-white">About</a>
-            <a href="#" className="dark:text-gray-300 dark:hover:text-white">Projects</a>
-            <a href="#" className="dark:text-gray-300 dark:hover:text-white">Contact</a>
-          </nav>
+          <div className="flex items-center space-x-6">
+            <LanguageSwitcher />
+            <NavItems />
+            <MobileMenu className="ml-4" />
+          </div>
         </div>
       </Container>
     </header>
