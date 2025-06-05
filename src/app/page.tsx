1eb6417985aec1c4891e7d6111a0e96e6d43ead1
Wenderson Junior <wenderson.junior@onovomercado.com.br>
2025-06-05 15:07:44 -0300

    fix: update root layout and page to prevent hydration errors with i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-import { Header } from "./components/header/header";
-import { Footer } from "./components/footer/footer";
-import { Container } from "./components/ui/Container";
+import { redirect } from 'next/navigation';
+import { defaultLocale } from '../i18n';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow py-8">
-        <Container>
-          Content
-        </Container>
-      </main>
-      <Footer />
-    </div>
-  );
+  redirect(`/${defaultLocale}`);
 }
