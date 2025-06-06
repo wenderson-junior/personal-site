a3ede397e3f1e0f492a708e672865de45d680613
Wenderson Junior <wenderson.junior@onovomercado.com.br>
2025-06-06 12:55:08 -0300

    fix: component name change

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Header } from "@/app/components/header/Header";
+import { Navbar } from "@/app/components/header/Navbar";
 import { Footer } from "@/app/components/footer/Footer";
 import { Hero } from "@/app/components/hero/Hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Navbar />
       <main className="flex-grow bg-gray-950 pt-24">
         <Hero />
       </main>
